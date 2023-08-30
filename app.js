7d1f3f58ff366808af69e3d8ddcc8abb944c8cee
Naufal Irsyad Ihsan <naufalirsd.ppqita@gmail.com>
2023-08-30 10:33:59 +0700

    update app.js

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+// req adalah singkatan dari request yang isinya dikirimkan oleh client.
+// contoh seperti body, parameter, query
+// res adalah singkatan dari respons yang isinya kita kirim ke client
+// contoh seperti data, json, html, dan codeHTTP (default:200)
+
 app.listen(3000, () => {
-    console.log("Hai Bang, Servermu udah jalan di localhost:3000");
+    console.log("Hai Bang, Servermu udah jalan di http://localhost:3000");
 });
 
 // Jalaninnya dengan menulis node app.js di terminal
